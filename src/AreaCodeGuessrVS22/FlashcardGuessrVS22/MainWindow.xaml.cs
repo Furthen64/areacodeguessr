f829e27bcfdae73acd9fa0bdc8ede57958d7bd86
Fayt64 <35999444+Furthen64@users.noreply.github.com>
2024-06-19 21:51:31 +0200

    Changed to dotnet 8.0 because the WPF compile errors were getting intense

--- src/AreaCodeGuessrVS22/FlashcardGuessrVS22/MainWindow.xaml.cs
+++ src/AreaCodeGuessrVS22/FlashcardGuessrVS22/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +19,132 @@ namespace FlashcardGuessrVS22
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string currentCountry = "";
+        private List<string> imagePaths = null;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("starting game now");
+        }
+
+        public bool ValidateInput(string inputStr)
+        {
+            bool correctInput = false;
+            if (inputStr == currentCountry)
+            {
+                correctInput = true;
+
+                // remove the country
+
+            }
+
+            return correctInput;
+        }
+
+        private void _okBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(ValidateInput(inputCountry.Text))
+            {
+                MessageBox.Show("Great!");       
+            }
+            else
+            {
+                MessageBox.Show("Wrong.");
+            }
+        }
+
+
+        private void showimage(string pathToPngFiles)
+        {
+           imagePaths = new List<string>();
+
+            try
+            {
+                var files = Directory.EnumerateFiles(pathToPngFiles);
+
+                foreach (string filename in files)
+                {
+                    string pattern = @"[a-z]+_[0-9]{4}[_a-zA-Z0-9]*\.(png|PNG)";
+                    RegexOptions options = RegexOptions.Singleline;
+
+                    //string item = "example_2024_image123.PNG"; // Example input string
+
+                    foreach (Match m in Regex.Matches(filename, pattern, options))
+                    {
+                        Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+                        imagePaths.Add(filename);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }      
+
+
+            // Now show it
+            if (imagePaths.Any())
+            {   
+                // Create a BitmapImage
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                string fullpath = imagePaths.Last();
+                bitmap.UriSource = new Uri(fullpath);
+                bitmap.EndInit();
+
+                // Create an Image
+                Image croppedImage = new Image();
+                croppedImage.Width = 400;
+                croppedImage.Margin = new Thickness(2);
+
+                // Create a CroppedBitmap from BitmapImage
+                if(bitmap.PixelWidth >= 400 && bitmap.PixelHeight>= 400)
+                {
+                    var scale = Math.Max(bitmap.PixelWidth, bitmap.PixelHeight)/400.0;
+
+                    var targetBitmap = new TransformedBitmap((BitmapSource)bitmap, new ScaleTransform(1/scale, 1/scale));
+
+                    croppedImage.Source = targetBitmap;    
+                }  
+                else
+                {
+                    
+                    croppedImage.Source = bitmap;
+                }
+
+                                         
+
+                // Add Image to Window
+                stackpanBottom.Children.Add(croppedImage);
+
+            }       
+
+            return;
+            
+        }
+        private void browseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string fullFolderPath = "";
+            var dlg = new FolderPicker(); 
+
+            dlg.InputPath = Properties.Settings.Default.LastOpenedFolder;   
+
+            if (dlg.ShowDialog() == true)
+            {
+                fullFolderPath = dlg.ResultPath;
+            } 
+
+            if(fullFolderPath != "")
+            {
+                Properties.Settings.Default.LastOpenedFolder = fullFolderPath;
+                Properties.Settings.Default.Save();
+                showimage(fullFolderPath);     
+            }
         }
     }
 }
